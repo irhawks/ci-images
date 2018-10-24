a96d42e364b716b45f00ba1bd65803f1963a2625
rencx <rencx@cetcocean.com>
2018-10-24 15:03:49 +0800

    update full

--- r/scripts/install_geo_packages.sh
+++ r/scripts/install_geo_packages.sh
@@ -2,4 +2,4 @@
 
 apt install -yq libudunits2-dev libv8-dev
 apt install -yq libcairo2-dev libcurl4-openssl-dev 
-Rscript -e install.packages('geosphere')
+Rscript -e "install.packages('geosphere')"
