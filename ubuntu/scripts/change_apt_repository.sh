bd59721f519df343300f86a9f0123e2f6f01e962
rencx <rencx@cetcocean.com>
2018-10-24 10:56:40 +0800

    debug

--- ubuntu/scripts/change_apt_repository.sh
+++ ubuntu/scripts/change_apt_repository.sh
@@ -7,6 +7,8 @@ cat /etc/apt/sources.list ## DEBUG
 sed -i  "s|http://archive.ubuntu.com|${UBUNTU_APT_REMOTE_NAME}|" /etc/apt/sources.list
 sed -i "s|http://security.ubuntu.com|${UBUNTU_APT_REMOTE_NAME}|" /etc/apt/sources.list
 
+cat /etc/apt/sources.list ## DEBUG
+
 ## e.g. wget/curl for download files from remote server
 apt update
 apt install -y wget curl apt-transport-https
